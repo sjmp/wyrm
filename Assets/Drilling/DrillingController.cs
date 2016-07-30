6d22716410b4c2690d38bef0939d9100715a0ab7
sjmp <samueljamespartridge@gmail.com>
2016-07-30 13:45:15 +0100

    First Unit commit

---
+++ Assets/Drilling/DrillingController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Drilling
+{
+    public class DrillingController : MonoBehaviour
+    {
+        public Text TitleText;
+        public Text BodyText;
+
+        // Use this for initialization
+        void Start ()
+        {
+            TitleText.text = "Myer & Son's Mechanised Drilltrain";
+            BodyText.text = "A mud caked industrial monstrosity, designed to hunt Wyrms in the depths of the earth";
+        }
+	
+        // Update is called once per frame
+        void Update () {
+	
+        }
+    }
+}
