2a620f4b5e846bee27d3396a33f18ffdd11dd217
sjmp <samueljamespartridge@gmail.com>
2016-08-02 21:53:08 +0100

    New Scenes, new buttons

---
+++ Assets/Main/MainController.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class MainController : MonoBehaviour
+{
+
+    public Image Background;
+
+    // Use this for initialization
+    void Start () {
+
+        if (SceneManager.sceneCount != 1)
+        {
+            GameObject go = (GameObject)Instantiate(Resources.Load("ResumeButton"));
+            go.transform.SetParent(Background.gameObject.transform, false);
+        }
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Quit()
+    {
+        Debug.Log("Quitting!");
+        Application.Quit();
+    }
+
+    public void Lore()
+    {
+        Debug.Log("Lore!");
+        SceneManager.LoadScene("LoreScene");
+    }
+
+    public void BeginGame()
+    {
+        Debug.Log("Starting!");
+        SceneManager.LoadScene("DrillingScene");
+        SceneManager.UnloadScene("MainScene");
+    }
+
+    public void ReturnToGame()
+    {
+        Debug.Log("Returning!");
+        SceneManager.UnloadScene("MainScene");
+    }
+}
